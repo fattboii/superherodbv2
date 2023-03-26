72dda322f778ccddeae5e3f4647197cfbc18ccd3
Josue Orozco Fregoso <josue.orozco1130@gmail.com>
2023-03-25 20:39:03 -0600

    Removed 'register' and 'reset password' options in the login page, aswell as adding mandatory login to enter the superhero database

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,11 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Auth::routes();
+Auth::routes(['register'=> false, 'reset' => false]);
 
 Route::get('/home', [HeroController::class, 'index'])->name('home');
 
-Route::resource('heros', HeroController::class);
+Route::resource('heros', HeroController::class) -> middleware('auth');
 
 Route::group(['middleware' => 'auth'], function(){
     Route::get('/home', [HeroController::class, 'index'])->name('home');
